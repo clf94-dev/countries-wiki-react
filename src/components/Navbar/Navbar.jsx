75adb28a3aec3439f9c407bea2d4a27a3b46acfc
DESKTOP-732UDSA\carmen <clf94@hotmail.es>
2023-12-26 09:50:21 +0100

    - add navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
+import styles from './Navbar.module.css'
 function Navbar(){
     return (
-        <div>Navbar Section</div>
+        <div className={styles.navContainer}>
+            <div className={styles.title}>
+                <h3>Countries Wiki</h3>
+            </div>
+            <div className={styles.btnSection}>
+                <button className={styles.darkModeBtn}>Dark Mode</button>
+            </div>
+        </div>
     )
 }
 
