5961303c7aadc1df998fb9d45a8bebd98d3ff498
DESKTOP-732UDSA\carmen <clf94@hotmail.es>
2023-12-26 11:32:46 +0100

    - add multiple cards map

--- src/components/CountriesList/CountriesList.jsx
+++ src/components/CountriesList/CountriesList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import styles from './CountriesList.module.css'
 
@@ -6,16 +6,16 @@ function CountriesList() {
     const [showCountryName, setShowCountryName] = useState(false)
     return(
         <div className={styles.listContainer}>
-            <div 
+            {[1, 2, 3].map(item =>  <div 
                 className={styles.card}
                 onMouseEnter={() => setShowCountryName(true)}
                 onMouseLeave={() => setShowCountryName(false)}
             >
-                <p className={styles.image}>Image</p>
+                <p className={styles.image}>Image {item}</p>
                 { showCountryName ? <div className={styles.countryOverlay}>
                     <p className={styles.name}>name</p>
                 </div> : null}
-            </div>
+            </div>)}
         </div>
     )
 }
