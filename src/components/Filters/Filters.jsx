26782bbdf68dc8da65082deb112f383ee01d6be9
DESKTOP-732UDSA\carmen <clf94@hotmail.es>
2023-12-26 19:07:11 +0100

    - add base filters components

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,7 +1,57 @@
+
+import styles from './Filters.module.css';
+import Select from 'react-select'
+
+import { useStore } from '../../store';
+
 function Filters() {
- return (
-    <div>Filters section</div>
- )
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+      ]
+   
+    const darkModeOn = useStore((state) => state.darkModeOn);
+    const customStyles = {
+        option: (defaultStyles, state) => ({
+          ...defaultStyles,
+          color: state.isSelected ? "#212529" : "#fff",
+          backgroundColor: state.isSelected ? "#a0a0a0" : "#212529",
+        }),
+    
+        control: (defaultStyles) => ({
+          ...defaultStyles,
+          backgroundColor: "#212529",
+          padding: "6px",
+          border: "none",
+          boxShadow: "none",
+        }),
+        singleValue: (defaultStyles) => ({ ...defaultStyles, color: "#fff" }),
+      };
+      const customStylesLight = {
+        option: (defaultStyles, state) => ({
+          ...defaultStyles,
+          color: state.isSelected ? "#212529" : "#000",
+          backgroundColor: state.isSelected ? "#a0a0a0" : "#fffff",
+        }),
+    
+        control: (defaultStyles) => ({
+          ...defaultStyles,
+          backgroundColor: "#fff",
+          padding: "6px",
+          border: "none",
+          boxShadow: "none",
+        }),
+        singleValue: (defaultStyles) => ({ ...defaultStyles, color: "#000" }),
+      };
+    return (
+       <div className={darkModeOn ? `${styles.filters} ${styles.dark}` : styles.filters}>
+            <input type="text" placeholder='Search for a country' className={styles.searchBar}  />
+           <Select placeholder="Select currency" styles={darkModeOn ? customStyles : customStylesLight} options={options} />
+           <Select placeholder="Select region" styles={darkModeOn ? customStyles : customStylesLight} options={options} />
+    
+       </div>
+    )
 }
 
 export default Filters;
