7e66710a90e9fda94cd125771406379ee5f0407c
DESKTOP-732UDSA\carmen <clf94@hotmail.es>
2023-12-26 11:22:29 +0100

    - add base card in countries list

--- src/components/CountriesList/CountriesList.jsx
+++ src/components/CountriesList/CountriesList.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react'
+
+import styles from './CountriesList.module.css'
+
 function CountriesList() {
+    const [showCountryName, setShowCountryName] = useState(false)
     return(
-        <div>CountriesList section</div>
+        <div className={styles.listContainer}>
+            <div 
+                className={styles.card}
+                onMouseEnter={() => setShowCountryName(true)}
+                onMouseLeave={() => setShowCountryName(false)}
+            >
+                <p className={styles.image}>Image</p>
+                { showCountryName ? <div className={styles.countryOverlay}>
+                    <p className={styles.name}>name</p>
+                </div> : null}
+            </div>
+        </div>
     )
 }
 export default CountriesList
