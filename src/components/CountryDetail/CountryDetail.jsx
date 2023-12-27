c6ba8f3dc3536b6f8c155c23f31846ef188f3bc9
DESKTOP-732UDSA\carmen <clf94@hotmail.es>
2023-12-27 11:26:16 +0100

    add base structure detail component (html)

--- src/components/CountryDetail/CountryDetail.jsx
+++ src/components/CountryDetail/CountryDetail.jsx
@@ -1,6 +1,34 @@
+import { useStore } from "../../store"
+
 function CountryDetail() {
+    const countryDetail = useStore((state) => state.countryDetail)
+    console.log({countryDetail})
     return(
-        <div>Country detail section</div>
+        <div>
+            <div>
+                <img src={countryDetail.flags.svg} alt={`${countryDetail.name.common} flag image`} />
+            </div>
+            <div>
+                <h3>{countryDetail.name.common}</h3>
+                <p><strong>Native name</strong>: {}</p>
+                <p><strong>Country Code</strong>: {countryDetail.cca3}</p>
+                <p><strong>Population</strong>: {countryDetail.population}</p>
+                <p><strong>Region</strong>: {countryDetail.region}</p>
+                <p><strong>Sub Region</strong>: {countryDetail.subregion}</p>
+                <p><strong>Capital</strong>: {countryDetail.capital[0]}</p>
+
+                <p><strong>Currency</strong>: {countryDetail.subregion}</p>
+                <p><strong>Timezone</strong>: {countryDetail.timezones?.map( timezone => timezone)}</p>
+                <div>
+                    <p>
+                        <strong>Borders</strong>
+                        :
+                    </p> 
+                    {countryDetail.borders?.map(border => <p>{border}</p>)}</div>
+
+
+            </div>
+        </div>
     )
 }
 
