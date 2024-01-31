e84b088114b6c40a414122bb1ad952965d0d8282
clf94-dev <clf94@hotmail.es>
2024-01-31 18:07:47 +0100

    - styled country detail

--- src/components/CountryDetail/CountryDetail.jsx
+++ src/components/CountryDetail/CountryDetail.jsx
@@ -1,6 +1,8 @@
 import { useStore } from "../../store"
 import { useNavigate } from "react-router-dom"
 
+import styles from './CountryDetail.module.css'
+
 function CountryDetail() {
     const navigate = useNavigate();
     const countryDetail = useStore((state) => state.countryDetail);
@@ -13,31 +15,33 @@ function CountryDetail() {
     }
     return(
         <div>
-            <div>
-                <button onClick={() => handleBackClick()}>Back</button>
+            <div className={styles.backRow}>
+                <button className={styles.backBtn} onClick={() => handleBackClick()}>Back</button>
             </div>
-            <div>
-                <img src={countryDetail.flags.svg} alt={`${countryDetail.name.common} flag image`} />
-            </div>
-            <div>
-                <h3>{countryDetail.name.common}</h3>
-                <p><strong>Native name</strong>: {}</p>
-                <p><strong>Country Code</strong>: {countryDetail.cca3}</p>
-                <p><strong>Population</strong>: {countryDetail.population}</p>
-                <p><strong>Region</strong>: {countryDetail.region}</p>
-                <p><strong>Sub Region</strong>: {countryDetail.subregion}</p>
-                <p><strong>Capital</strong>: {countryDetail.capital[0]}</p>
+            <div className={styles.countryInfoContainer}>
+                <div className={styles.imageColumn}>
+                    <img className={styles.countryFlag} src={countryDetail.flags.svg} alt={`${countryDetail.name.common} flag image`} />
+                </div>
+                <div className={styles.InfoColumn}>
+                    <h3 className={styles.name}>{countryDetail.name.common}</h3>
+                    <p className={styles.nativeName}><strong>Native name</strong>: {}</p>
+                    <p className={styles.code}><strong>Country Code</strong>: {countryDetail.cca3}</p>
+                    <p className={styles.population}><strong>Population</strong>: {countryDetail.population}</p>
+                    <p className={styles.region}><strong>Region</strong>: {countryDetail.region}</p>
+                    <p className={styles.subregion}><strong>Sub Region</strong>: {countryDetail.subregion}</p>
+                    <p className={styles.capital}><strong>Capital</strong>: {countryDetail.capital[0]}</p>
 
-                <p><strong>Currency</strong>: {countryDetail.subregion}</p>
-                <p><strong>Timezone</strong>: {countryDetail.timezones?.map( timezone => timezone)}</p>
-                <div>
-                    <p>
-                        <strong>Borders</strong>
-                        :
-                    </p> 
-                    {countryDetail.borders?.map(border => <p>{border}</p>)}</div>
+                    <p className={styles.currency}><strong>Currency</strong>: {countryDetail.subregion}</p>
+                    <p className={styles.timezone}><strong>Timezone</strong>: {countryDetail.timezones?.map( timezone => timezone)}</p>
+                    <div>
+                        <p>
+                            <strong>Borders</strong>
+                            :
+                        </p> 
+                        {countryDetail.borders?.map(border => <p>{border}</p>)}</div>
 
 
+                </div>
             </div>
         </div>
     )
