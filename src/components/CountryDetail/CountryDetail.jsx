0b22e0e45792b8e15fbcf6969b61577142b75ac5
clf94-dev <clf94@hotmail.es>
2024-02-01 06:08:54 +0100

    - add detail style desktop & darkMode

--- src/components/CountryDetail/CountryDetail.jsx
+++ src/components/CountryDetail/CountryDetail.jsx
@@ -6,6 +6,7 @@ import styles from './CountryDetail.module.css'
 function CountryDetail() {
     const navigate = useNavigate();
     const countryDetail = useStore((state) => state.countryDetail);
+    const darkModeOn = useStore((state) => state.darkModeOn);
     const resetCountryDetail = useStore((state) => state.resetCountryDetail)
     console.log({countryDetail})
 
@@ -14,31 +15,33 @@ function CountryDetail() {
         resetCountryDetail()
     }
     return(
-        <div>
+        <div className={darkModeOn ? `${styles.darkDetailContainer} ${styles.detailContainer}` : styles.detailContainer}>
             <div className={styles.backRow}>
-                <button className={styles.backBtn} onClick={() => handleBackClick()}>Back</button>
+                <button className={darkModeOn?  `${styles.darkBackButton} ${styles.backBtn}`  :  styles.backBtn} onClick={() => handleBackClick()}> <ion-icon className={styles.arrowIcon} name="arrow-back-outline" size="medium"></ion-icon>Back</button>
             </div>
             <div className={styles.countryInfoContainer}>
                 <div className={styles.imageColumn}>
                     <img className={styles.countryFlag} src={countryDetail.flags.svg} alt={`${countryDetail.name.common} flag image`} />
                 </div>
-                <div className={styles.InfoColumn}>
-                    <h3 className={styles.name}>{countryDetail.name.common}</h3>
-                    <p className={styles.nativeName}><strong>Native name</strong>: {}</p>
-                    <p className={styles.code}><strong>Country Code</strong>: {countryDetail.cca3}</p>
-                    <p className={styles.population}><strong>Population</strong>: {countryDetail.population}</p>
-                    <p className={styles.region}><strong>Region</strong>: {countryDetail.region}</p>
-                    <p className={styles.subregion}><strong>Sub Region</strong>: {countryDetail.subregion}</p>
-                    <p className={styles.capital}><strong>Capital</strong>: {countryDetail.capital[0]}</p>
-
-                    <p className={styles.currency}><strong>Currency</strong>: {countryDetail.subregion}</p>
-                    <p className={styles.timezone}><strong>Timezone</strong>: {countryDetail.timezones?.map( timezone => timezone)}</p>
-                    <div>
+                <div className={darkModeOn ? `${styles.darkInfoColumn} ${styles.infoColumn}` : styles.infoColumn}>
+                    <h1>{countryDetail.name.common}</h1>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Native name</strong>: {}</p>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Country Code</strong>: {countryDetail.cca3}</p>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Population</strong>: {countryDetail.population}</p>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Region</strong>: {countryDetail.region}</p>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Sub Region</strong>: {countryDetail.subregion}</p>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Capital</strong>: {countryDetail.capital[0]}</p>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Currency</strong>: {countryDetail.subregion}</p>
+                    <p className={styles.detailParagrah}><strong className={styles.detailTitle}>Timezone</strong>: {countryDetail.timezones?.map( timezone => timezone)}</p>
+                    <div className={styles.borders}>
                         <p>
-                            <strong>Borders</strong>
+                            <strong className={styles.detailTitle}>Borders</strong>
                             :
                         </p> 
-                        {countryDetail.borders?.map(border => <p>{border}</p>)}</div>
+                        <div className={styles.borderItems}>
+                            {countryDetail.borders?.map(border => <div className={darkModeOn ? `${styles.darkBorderItem} ${styles.borderItem}` :styles.borderItem}>{border}</div>)}
+                        </div>
+                    </div>
 
 
                 </div>
