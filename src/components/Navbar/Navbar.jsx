2f92e0fb696a4f70437e7bd9b69c509035944626
clf94-dev <clf94@hotmail.es>
2024-02-02 07:52:23 +0100

    - change language with switch

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import PropTypes from 'prop-types'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next';
+import Switch from "react-switch";
 import styles from './Navbar.module.css'
-
+import EnglishIcon from '../../assets/sh.svg'
+import SpanishIcon from '../../assets/es.svg'
 import { useStore } from '../../store'
 
 function Navbar(){
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+    const [languageSelected, setLanguageSelected] = useState(i18n.language === 'es')
     const darkModeOn = useStore((state) => state.darkModeOn);
     const activateDarkMode= useStore((state) => state.activateDarkMode);
     const deactivateDarkMode= useStore((state) => state.deactivateDarkMode);
@@ -14,13 +17,28 @@ function Navbar(){
         if (darkModeOn) deactivateDarkMode()
         else activateDarkMode()
     }
-    console.log({darkModeOn})
+  
+    const handleChangeLanguage = (e)  => {
+      i18n.changeLanguage(e ? "es" : "en");
+      localStorage.setItem('i18nextLng', e ? "es" : "en")
+      setLanguageSelected(e)
+    }
+    
     return (
         <div className={darkModeOn ? `${styles.navContainer} ${styles.darkContainer}`: styles.navContainer}>
             <div className={styles.title}>
                 <h3>{t('title')}</h3>
             </div>
             <div className={styles.btnSection}>
+            <img src={EnglishIcon} alt="english language icon" />
+            <Switch
+                onChange={handleChangeLanguage}
+                checked={languageSelected} 
+                checkedIcon={false}
+                uncheckedIcon={false}
+               
+            />
+            <img src={SpanishIcon} alt="spanish language icon" />
                 <button className={darkModeOn ?  `${styles.darkModeBtn} ${styles.active}` : styles.darkModeBtn} onClick={handleClickMode}>
                     {darkModeOn ? <ion-icon name='sunny-outline' size="small"></ion-icon> : <ion-icon name='moon-outline' size="small"></ion-icon>}
                     <p>Dark Mode</p>
@@ -30,7 +48,4 @@ function Navbar(){
     )
 }
 
-Navbar.propTypes = {
-
-}
-export default Navbar
+export default Navbar;
