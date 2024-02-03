3b67b8d959c2cb936fb2639a15d42a19697f0619
clf94-dev <clf94@hotmail.es>
2024-02-03 08:28:30 +0100

    -responsive country page components

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -1,9 +1,10 @@
 import CountryDetail from "../../components/CountryDetail"
 import Navbar from "../../components/Navbar"
+import styles from './Country.module.css'
 
 function Country () {
     return (
-        <div style={{display: 'flex', flexDirection:'column', justifyContent:'center'}}>
+        <div className={styles.countryContainer}>
             <Navbar />
             <CountryDetail />
         </div>
