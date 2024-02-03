d2d0140ef97198de66c94993ee37401d33fa0e12
clf94-dev <clf94@hotmail.es>
2024-02-03 18:43:21 +0100

    - add redirection link not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,12 @@
-
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 function NotFound () {
+   const { t } = useTranslation();
  return (
-    <div>not found page</div>
+    <div style={{display: 'flex', flexDirection: 'column', height: '200px', justifyContent: 'center', alignItems: 'center'}}>
+      <h1>Not found page</h1>
+      <Link to="/">{t('notFoundPageLink')}</Link>
+    </div>
  )
 }
 export default NotFound;
